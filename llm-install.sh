24bdf27d28eb5eb5d95dce38264ee1f02843b9f2
Kjell Wooding <kjell@wooding.org>
2023-11-21 12:16:57 -0500

    start processing texts into sentences

--- llm-install.sh
+++ llm-install.sh
@@ -4,3 +4,5 @@ llm sentence-transformers register intfloat/e5-large-v2 -a lv2
 llm install llm-mlc
 llm mlc setup
 llm mlc download-model Llama-2-7b-chat --alias llama2
+python -m spacy download en_core_web_sm
+python -m ipykernel install --user --name=llm_ai_eo
